48cb1226c384c99c8e926df23ed90360bb4df764
brucezhu <brucezhu@kuai.ma>
2016-04-11 04:38:32 +0800

    add

--- lib/omniauth/strategies/kuaima.rb
+++ lib/omniauth/strategies/kuaima.rb
@@ -3,34 +3,22 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Kuaima < OmniAuth::Strategies::OAuth2
-      option :client_options, {
-        :site => 'https://i.kuai.ma/oauth/v1',
-        :authorize_url => 'https://i.kuai.ma/oauth/authorize',
-        :token_url => 'https://i.kuai.ma/oauth/access_token'
-      }
 
-      def request_phase
-        super
-      end
+      # Give your strategy a name.
+      option :name, "kuaima"
 
-      def authorize_params
-        super.tap do |params|
-          %w[scope client_options].each do |v|
-            if request.params[v]
-              params[v.to_sym] = request.params[v]
-            end
-          end
-        end
-      end
+      # This is where you pass the options you would pass when
+      # initializing your consumer from the OAuth gem.
+      option :client_options, {:site => "https://i.kuai.ma/oauth/v1", :authorize_url => 'https://i.kuai.ma/oauth/authorize', :token_url => 'https://i.kuai.ma/oauth/access_token'}
 
-      uid { raw_info['id'].to_s }
+      uid { raw_info['id'] }
 
       info do
         {
-          'nickname' => raw_info['full_name'],
-          'email' => raw_info['email'],
-          'name' => raw_info['code'],
-          'image' => raw_info['avatar_url']
+          :nickname => raw_info['full_name'],
+          :email => raw_info['email'],
+          :name => raw_info['code'],
+          :image => raw_info['avatar_url']
         }
       end
 
@@ -42,6 +30,7 @@ module OmniAuth
         access_token.options[:mode] = :query
         @raw_info ||= access_token.get('user').parsed
       end
+
     end
   end
 end
