1b653d64b0b8c800a9ddd5f52c8d28d5b1f5179b
brucezhu <brucezhu@kuai.ma>
2016-04-13 00:43:46 +0800

    add

--- lib/omniauth/strategies/kuaima.rb
+++ lib/omniauth/strategies/kuaima.rb
@@ -33,9 +33,9 @@ module OmniAuth
 
       private
 
-      def callback_url
-        options.redirect_url || (full_host + script_name + callback_path)
-      end
+      #def callback_url
+      #  options.redirect_url || (full_host + script_name + callback_path)
+      #end
     end
   end
 end
